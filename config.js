81772ddfaf35462101b15b5d13a7bbacd6f79df6
Anam Anonymous <anamtradervx@gmail.com>
2025-12-28 00:37:03 +0700

    Add config.js

---
+++ config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  // Token bot dari @BotFather
+  BOT_TOKEN: "8231473468:AAE8-3i3jbATdBBZtWCAzX-dn6mFN5NKi0Q",
+  
+  // ID pemilik bot (dapatkan dari @userinfobot di Telegram)
+  OWNER_ID: 7772847824,
+  
+  BOT_NAME: "Broadcast-main",
+  AUTHOR: "@PraiOfficial",
+  
+  // Interval auto broadcast (dalam detik)
+  AUTO_BC_INTERVAL: 200, // 1 jam
+  
+  // File penyimpanan data
+  DATA_FILE: "./data.json",
+  PREMIUM_FILE: "./premium.json",
+  
+  // Pengaturan premium
+  PREMIUM_FEATURES: {
+    AUTO_BROADCAST: true,
+    MAX_GROUPS: 1000
+  },
+  
+  // Prefix command
+  PREFIX: "/"
+};
+
+/*
+AUTHOR  : @dliinotdev
+
+SUPPORT : AlwaysDlii [ ME ]
+          : Deepseek AI
+          : frmnzz.json
+          : XTRX | DITTH Xyace
+          : Archi
+          : All Patrner Dlii
+          : All Buyyer Script
+NOTE : NO DELETE TEKS INI,HARGAILAH PEMBUAT SCRIPT
+*/
